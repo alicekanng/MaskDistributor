783d1b9be02b889955c9f59954dc0b3469a0c903
Alice <alicekanng@gmail.com>
2020-07-27 02:27:17 -0700

    Implemented main, LocalApplication, ForeignApplication in ui package.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,85 +1,41 @@
 package ui;
 
-import exceptions.CustomerDidNotReceiveMasksException;
-import exceptions.CustomerNotInListException;
 import model.Customer;
-import model.ForeignList;
-import model.LocalList;
+import model.DistributionList;
+
+import java.util.Scanner;
 
 public class Main {
+
+    private Scanner sc;
+    private DistributionList list;
+    private Customer customer;
+
     public static void main(String[] args) {
-        LocalList localList = new LocalList();
-        ForeignList foreignList = new ForeignList();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter customer information (making sure to capitalize!):");
 
-        Customer alice = new Customer("Alice Kang",
-                "North Vancouver, BC", 19, "None");
-        Customer jennifer = new Customer("Jennifer Aniston",
-                "Toronto, ON", 30, "Pregnant");
-        Customer steve = new Customer("Steve Martin",
-                "Kitsilano, BC", 80, "None");
-        Customer robert = new Customer("Robert Deniro",
-                "San Jose, CA", 78, "Alzheimer's");
-        Customer martin = new Customer("Martin Freeman",
-                "Richmond, BC", 10, "None");
-        Customer emily = new Customer("Emily Blunt",
-                "Surrey, BC", 12, "Asthma");
+        System.out.println("Please enter the customer's name:");
+        String name = sc.nextLine();
 
+        System.out.println("Please enter the customer's address in the format 'City, Province':");
+        String address = sc.nextLine();
 
-        //add customers to a distribution list according to their address
-        System.out.println("Adding customers into appropriate distribution lists.");
-        localList.addCustomer(alice);
-        localList.addCustomer(steve);
-        localList.addCustomer(martin);
-        foreignList.addCustomer(jennifer);
-        foreignList.addCustomer(robert);
-        foreignList.addCustomer(emily);
-        System.out.println("Local customers in BC have been added to the local list.");
-        System.out.println("Foreign customers out of BC have been added to the foreign list.");
+        System.out.println("Please enter the customer's age:");
+        int age = sc.nextInt();
 
-        //get number of masks given to alice, jennifer, robert
-        try {
-            localList.getNumberOfMasksCustomer(alice);
-            foreignList.getNumberOfMasksCustomer(jennifer);
-            foreignList.getNumberOfMasksCustomer(robert);
-        } catch (CustomerNotInListException e) {
-            System.err.println("It looks like the given customer is not in this list.");
+        System.out.println("Please enter the customer's medical conditions, if none, enter 'None':");
+        String conditions = sc.next();
+
+        System.out.println("Creating a new customer with given information:");
+        Customer customer = new Customer(name, address, age, conditions);
+
+        if (address.contains("BC")) {
+            LocalApplication localApp = new LocalApplication(customer);
+            localApp.handleUserInput();
+        } else {
+            ForeignApplication foreignApp = new ForeignApplication(customer);
+            foreignApp.handleUserInput();
         }
-        System.out.println("The number of masks given to customer "
-                + alice.getName() + " is " + alice.getMasks() + ".");
-
-        System.out.println("The number of masks given to customer "
-                + jennifer.getName() + " is " + jennifer.getMasks() + ".");
-
-        System.out.println("The number of masks given to customer "
-                + robert.getName() + " is " + robert.getMasks() + ".");
-
-        //get date alice and robert will receive their masks by
-        try {
-            localList.getDateCustomer(alice);
-            foreignList.getDateCustomer(robert);
-        } catch (CustomerNotInListException e) {
-            System.err.println("It looks like the given customer is not in this list.");
-        }
-        System.out.println("Customer " + alice.getName() + " will receive their masks in "
-                + alice.getReceiveDate() + " days.");
-
-        System.out.println("Customer " + robert.getName() + " will receive their masks in "
-                + robert.getReceiveDate() + " days.");
-
-        //delete alice and robert from their lists
-        try {
-            localList.deleteCustomer(alice);
-            foreignList.deleteCustomer(robert);
-        } catch (CustomerNotInListException e) {
-            System.err.println("It looks like the given customer is not in this list.");
-        } catch (CustomerDidNotReceiveMasksException e) {
-            System.err.println("It looks like the given customer has not received their masks yet.");
-        }
-        System.out.println("Customer " + alice.getName() + " has been removed from the distribution list.");
-        System.out.println("Customer " + robert.getName() + " has been removed from the distribution list.");
-
-        //print list
-        System.out.println("Printing the local list:");
-        localList.printList();
     }
 }
